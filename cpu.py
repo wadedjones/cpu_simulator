7d77cfb6d3107720a9dbe5e385b9abf93052678d
Wade Jones <wadedjones@gmail.com>
2022-11-14 19:49:58 -0800

    added memory data input, added cpu read instructions

--- cpu.py
+++ cpu.py
@@ -1,1 +1,21 @@
 # CPU file
+
+from memory import Memory
+
+class CPU:
+    def __init__(self, data_file, instruction_file):
+        self.data_file = data_file
+        self.instruction_file = instruction_file
+        self.memory_bus = Memory()
+        self.memory_bus.data_input(self.data_file)
+        self.instructions = self.read_instructions()
+
+    def read_instructions(self):
+        instruction_list = []
+        with open(self.instruction_file, 'r') as file:
+            for line in file.readlines():
+                instruction_list.append(tuple(line.strip().split(',')))
+        return instruction_list
+
+    def execute_instructions(self):
+        pass
--- memory.py
+++ memory.py
@@ -3,4 +3,13 @@
 
 class Memory:
     def __init__(self):
-        self.bus = {'{0:08b}'.format(x): 0 for x in range(256)}
+        self.size = 128
+        self.bus = {'{0:08b}'.format(x): 0 for x in range(self.size)}
+
+    def data_input(self, data):
+        with open(data, 'r') as file:
+            for line in file.readlines():
+                memory_bus, value = line.strip().split(',')
+                self.bus[memory_bus] = value
+                print(f"Adding value: {value} to memory bus: {memory_bus}")
+
